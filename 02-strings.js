5067160ed7d26ef5c501051b04d2ec19d2ed3930
Tobias Knuth <tknuth@users.noreply.github.com>
2024-10-18 22:20:38 +0200

    added strings file

---
+++ 02-strings.js
@@ -0,0 +1,32 @@
+const s = "Hallo Welt!";
+
+// Der Index beginnt bei 0.
+s[0];
+
+// Slicing bildet ein Intervall [a:b).
+s.slice(3, 5);
+
+// Mit Template-Strings können Texte mit Variablen befüllt werden.
+const greeting = "Guten Morgen";
+const name = "Anna"`${greeting}, ${name}`;
+
+// Wiederholung von Strings
+"ab".repeat(3) + "c";
+
+let message = " HiER ist eTwAS schieFgegangen.";
+message.trim().toLowerCase().replace("etwas", "nichts");
+
+["a", "b", "c"].join(", ");
+
+"Dies ist ein Satz, der auch ein Komma enthält.".split(", ");
+
+// Passen Sie den Code an, sodass die Elemente sortiert ausgegeben werden.
+let l = "Apfel, Kiwi, Banane, Avocado, Kirsche, Pflaume";
+
+// Prüfen Sie, ob die beiden Wörter ein Anagramm bilden.
+const a = "Lehm";
+const b = "Mehl";
+
+// Berechnen Sie die Zeitdifferenz zwischen a und b in Minuten.
+const a = "17:45";
+const b = "19:15";
