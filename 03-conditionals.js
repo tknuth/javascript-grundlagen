24964ef28899ffc77a9d986cf70b88b934357382
Tobias Knuth <5556920+tknuth@users.noreply.github.com>
2025-05-26 15:43:59 +0200

    avoid browser functions in examples

--- 03-conditionals.js
+++ 03-conditionals.js
@@ -6,7 +6,7 @@ console.log(typeof true);
 1 >= 0;
 
 // `includes` prüft, ob ein Element in einem Array enthalten ist.
-console.log(["a", "b", "c"].includes("a"));
+["a", "b", "c"].includes("a");
 
 let weather = "Regen";
 let action;
@@ -28,15 +28,15 @@ let b = 2;
 let c = a > b ? 3 : 4;
 
 // Truthy/Falsy und Short-Circuiting
-console.log(a || b);
-console.log(a && b);
+a || b;
+a && b;
 
 // Vervollständigen Sie das Programm.
-let x = prompt("Nenne eine Zahl:");
-console.log("Die Zahl ist gerade.");
-console.log("Die Zahl ist ungerade.");
+let x = 7;
+"Die Zahl ist gerade.";
+"Die Zahl ist ungerade.";
 
 // Programmieren Sie Schere, Stein, Papier.
 let choices = ["Schere", "Stein", "Papier"];
-let player = prompt("Schere, Stein oder Papier?");
+let player = "Schere";
 let computer = choices[Math.floor(Math.random() * choices.length)];
